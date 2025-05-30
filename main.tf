32c2250dd99ac755afae8a0459fe201fc8420977
LemonLucy <eungyo0965@gmail.com>
2025-05-30 11:09:10 +0900

    add service role

--- main.tf
+++ main.tf
@@ -89,3 +89,47 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket        = "${var.service_name}-artifacts-${random_id.bucket_suffix.hex}"
   force_destroy = true
 }
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy_attach" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codepipeline.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy_attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodePipelineFullAccess"
+}
