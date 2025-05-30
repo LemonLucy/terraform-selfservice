1c97ef0bdbca3846e591c2ad5ae737b23a1f48bb
LemonLucy <eungyo0965@gmail.com>
2025-05-30 11:26:25 +0900

    fix: edit resource name

--- main.tf
+++ main.tf
@@ -2,34 +2,37 @@ resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
-    Name = "${var.instance_name}" 
+    Name = var.instance_name
   }
 }
-# 1. AWS provider 설정
+
 provider "aws" {
   region = var.aws_region
 }
-# 2. ECR 생성 (도커 이미지 저장소)
+
 resource "aws_ecr_repository" "app_repo" {
   name = "${var.ecr_repo_name}-${var.service_name}"
 }
 
-# 3. CodeBuild 프로젝트
 resource "aws_codebuild_project" "app_build" {
-  name          = "build-${var.service_name}"
-  service_role  = aws_iam_role.codebuild_role.arn
+  name         = "build-${var.service_name}"
+  service_role = aws_iam_role.codebuild_role.arn
+
   source {
-    type      = "GITHUB"
-    location  = var.app_repo_url
+    type     = "GITHUB"
+    location = var.app_repo_url
   }
+
   artifacts {
     type = "NO_ARTIFACTS"
   }
+
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/standard:5.0"
     type                        = "LINUX_CONTAINER"
     privileged_mode             = true
+
     environment_variable {
       name  = "REPO_URI"
       value = aws_ecr_repository.app_repo.repository_url
@@ -37,15 +40,15 @@ resource "aws_codebuild_project" "app_build" {
   }
 }
 
-# 4. CodePipeline 구성
 resource "aws_codepipeline" "pipeline" {
   name     = "pipeline-${var.service_name}"
   role_arn = aws_iam_role.codepipeline_role.arn
+
   artifact_store {
-    # location = var.s3_bucket
     location = aws_s3_bucket.codepipeline_artifacts.bucket
     type     = "S3"
   }
+
   stage {
     name = "Source"
     action {
@@ -56,13 +59,14 @@ resource "aws_codepipeline" "pipeline" {
       version          = "1"
       output_artifacts = ["source_output"]
       configuration = {
-        Owner  = var.github_owner
-        Repo   = var.github_repo
-        Branch = "main"
+        Owner      = var.github_owner
+        Repo       = var.github_repo
+        Branch     = "main"
         OAuthToken = var.github_token
       }
     }
   }
+
   stage {
     name = "Build"
     action {
@@ -80,7 +84,6 @@ resource "aws_codepipeline" "pipeline" {
   }
 }
 
-# s3 bucket for cicd
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
@@ -114,7 +117,6 @@ resource "aws_iam_role_policy_attachment" "codebuild_policy_attach" {
 
 resource "aws_iam_role" "codepipeline_role" {
   name = "codepipeline-role-${var.service_name}"
-
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
